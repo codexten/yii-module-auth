80b6c3c6dd083114f214531e48cc04885c87b072
Jomon Johnson <jomon.entero@gmail.com>
2019-05-26 22:06:55 +0530

    init

---
+++ src/admin/models/AuthModuleSettings.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace codexten\yii\modules\auth\admin;
+
+use codexten\yii\settings\models\BaseSettingsModel;
+
+/**
+ * Class AuthModuleSettings
+ *
+ * @property boolean $forcePhoneNumberVerification
+ *
+ * @package codexten\yii\modules\auth\admin
+ */
+class AuthModuleSettings extends BaseSettingsModel
+{
+
+}
